00f18b52fb77c1d59647bb0e1463bd2b2eb9d8bd
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-07-24 22:11:33 +0100

    added react project for test

---
+++ _layouts/precache-manifest.8e9da36382c27b4feafbac39842f0203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d196ae493eb63ebe0d5396d14975f1d",
+    "url": "/car-rental-website/index.html"
+  },
+  {
+    "revision": "3311b50dc76bad1a2ccc",
+    "url": "/car-rental-website/static/css/2.d19c8617.chunk.css"
+  },
+  {
+    "revision": "770ebaa969907581fc64",
+    "url": "/car-rental-website/static/css/main.645b256b.chunk.css"
+  },
+  {
+    "revision": "3311b50dc76bad1a2ccc",
+    "url": "/car-rental-website/static/js/2.51e47f2e.chunk.js"
+  },
+  {
+    "revision": "770ebaa969907581fc64",
+    "url": "/car-rental-website/static/js/main.3ef9a09f.chunk.js"
+  },
+  {
+    "revision": "3ff55d17818c82a72517",
+    "url": "/car-rental-website/static/js/runtime~main.2596897a.js"
+  }
+]);
